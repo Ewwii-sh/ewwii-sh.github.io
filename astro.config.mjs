6ca716e961e96a82b3bfe58d09fdbd9bb716445b
Byson94 <byson94@outlook.com>
2025-09-06 11:38:56 +0530

    feat: redesiging FULL WEBSITE | removed starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/",
-  integrations: [
-    starlight({
-      title: "Ewwii",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/Ewwii-sh/",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Introduction",
-          link: "/docs/introduction",
-        },
-        {
-          label: "Getting Started",
-          autogenerate: { directory: "docs/getting-started" },
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "docs/reference" },
-        },
-      ],
-    }),
-  ],
-  redirects: {
-    "/docs": "/docs/introduction",
-    "/docs/getting-started": "/docs/getting-started/installation",
-    "/docs/reference": "/docs/reference/table_of_contents",
-    "/docs/configuration": "/docs/configuration/configuration",
-    "/docs/package-manager": "/docs/package-manager/ewwii-package-manager",
-  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
