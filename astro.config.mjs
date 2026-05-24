e658d4f9318c46dd2f668b8f4fe709c84c2dc291
Byson94 <byson94@outlook.com>
2026-05-24 18:00:06 +0530

    chore: set trailingslash to always

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ewwii-sh.github.io",
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
