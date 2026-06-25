ba42cd5bf74fd4019dc8f554913fbdc4e268326d
Byson94 <byson94@outlook.com>
2026-06-25 17:34:02 +0530

    chore: add nbcl language support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,30 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+import fs from 'node:fs'
+import path from 'node:path'
+
+const __dirname = path.resolve(path.dirname(''));
+const nbclGrammar = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, './nbcl.tmLanguage.json'), 'utf8')
+)
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ewwii-sh.github.io",
-  trailingSlash: 'always',
-  integrations: [sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    site: "https://ewwii-sh.github.io",
+    trailingSlash: 'always',
+    integrations: [sitemap()],
+    markdown: {
+        shikiConfig: {
+            langs: [{
+                name: 'nbcl',
+                aliases: ['nbcl'],
+                scopeName: nbclGrammar.scopeName,
+                ...nbclGrammar
+            }],
+        }
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
